6070d27e21cd9031c1e83d75db001493fe5e1e38
Alisher Bekenov <40290814+alisherbekenov@users.noreply.github.com>
2021-03-14 11:46:52 +0600

    Delete sites directory

--- sites/app.js
+++
@@ -1,4 +0,0 @@
-var quadimages = document.querySelectorAll("#quad figure");
-for(i=0; i<quadimages.length; i++) {
-  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") }); 
-}
